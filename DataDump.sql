fd57bd56e35fd8ec5283607bbf7f16d3f41bcbf2
Awareqwx <benpeters97@yahoo.com>
2017-12-18 15:38:46 -0800

    Fixed which schema was dumped. Oops.

--- DataDump.sql
+++ DataDump.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `dojoactivities` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `dojoactivities`;
+CREATE DATABASE  IF NOT EXISTS `profnetwork` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `profnetwork`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: dojoactivities
+-- Host: localhost    Database: profnetwork
 -- ------------------------------------------------------
 -- Server version	5.7.19-log
 
@@ -18,66 +18,33 @@ USE `dojoactivities`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `activities`
+-- Table structure for table `connections`
 --
 
-DROP TABLE IF EXISTS `activities`;
+DROP TABLE IF EXISTS `connections`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `activities` (
-  `ActivityId` int(11) NOT NULL AUTO_INCREMENT,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `Title` varchar(45) DEFAULT NULL,
-  `TimeAndDate` datetime DEFAULT NULL,
-  `Duration` int(11) DEFAULT NULL,
-  `DurationType` varchar(45) DEFAULT NULL,
-  `Description` text,
-  `CreatorId` int(11) NOT NULL,
-  PRIMARY KEY (`ActivityId`),
-  UNIQUE KEY `ActivityId_UNIQUE` (`ActivityId`),
-  KEY `fk_events_users_idx` (`CreatorId`),
-  CONSTRAINT `fk_events_users` FOREIGN KEY (`CreatorId`) REFERENCES `users` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `activities`
---
-
-LOCK TABLES `activities` WRITE;
-/*!40000 ALTER TABLE `activities` DISABLE KEYS */;
-INSERT INTO `activities` VALUES (1,'2017-12-15 12:08:54','2017-12-15 12:08:54','Generic Party','2017-12-30 12:00:00',1,'0','An extremely generic party. There is nothing special about it in the slightest.',1),(2,'2017-12-15 12:36:17','2017-12-15 12:36:17','another party','2017-12-20 03:15:00',1,'0','asdfasdfasdf',1),(4,'2017-12-15 13:23:43','2017-12-15 13:23:43','YEAAAHH!!!','2017-12-18 05:00:00',5,'0','WOOOOOOO!!!!!',2),(5,'2017-12-15 13:24:33','2017-12-15 13:24:33','Nerf War','2017-12-23 13:00:00',3,'0','Bring yo\' guns, we\'re gonna have some fun!',3),(6,'2017-12-15 14:35:31','2017-12-15 14:35:31','asdf','2017-12-22 11:11:00',5,'0','asdfasdfasdf',1);
-/*!40000 ALTER TABLE `activities` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `signups`
---
-
-DROP TABLE IF EXISTS `signups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `signups` (
-  `SignupId` int(11) NOT NULL AUTO_INCREMENT,
-  `ActivityId` int(11) NOT NULL,
-  `UserId` int(11) NOT NULL,
-  PRIMARY KEY (`SignupId`),
-  KEY `fk_events_has_users_users1_idx` (`UserId`),
-  KEY `fk_events_has_users_events1_idx` (`ActivityId`),
-  CONSTRAINT `fk_events_has_users_events1` FOREIGN KEY (`ActivityId`) REFERENCES `activities` (`ActivityId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_events_has_users_users1` FOREIGN KEY (`UserId`) REFERENCES `users` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `connections` (
+  `ConnectionId` int(11) NOT NULL AUTO_INCREMENT,
+  `ConnectorId` int(11) DEFAULT NULL,
+  `ConnectedToId` int(11) DEFAULT NULL,
+  `Accepted` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ConnectionId`),
+  KEY `fk_Users_has_Users_Users2_idx` (`ConnectedToId`),
+  KEY `fk_Users_has_Users_Users1_idx` (`ConnectorId`),
+  CONSTRAINT `fk_Users_has_Users_Users1` FOREIGN KEY (`ConnectorId`) REFERENCES `users` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Users_has_Users_Users2` FOREIGN KEY (`ConnectedToId`) REFERENCES `users` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `signups`
+-- Dumping data for table `connections`
 --
 
-LOCK TABLES `signups` WRITE;
-/*!40000 ALTER TABLE `signups` DISABLE KEYS */;
-INSERT INTO `signups` VALUES (1,1,2),(3,2,3),(6,2,2),(7,4,3),(8,1,3),(9,5,2),(10,5,1),(11,5,4);
-/*!40000 ALTER TABLE `signups` ENABLE KEYS */;
+LOCK TABLES `connections` WRITE;
+/*!40000 ALTER TABLE `connections` DISABLE KEYS */;
+INSERT INTO `connections` VALUES (3,2,3,1),(6,4,3,1),(7,1,2,1),(9,4,1,0),(11,2,4,0);
+/*!40000 ALTER TABLE `connections` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,12 +58,12 @@ CREATE TABLE `users` (
   `UserId` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `FirstName` varchar(45) DEFAULT NULL,
-  `LastName` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
   `Email` varchar(45) DEFAULT NULL,
   `PasswordHash` varchar(255) DEFAULT NULL,
+  `Description` text,
   PRIMARY KEY (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +72,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'2017-12-15 10:32:55','2017-12-15 10:32:55','John','Smith','johnsmith@foo.bar','AQAAAAEAACcQAAAAEI6okHYadOah95W2Xp7vK8mrREwhzk+5Ltmce5t9WZpTe2LTYfRdOMK2fTBpw94D/g=='),(2,'2017-12-15 10:50:09','2017-12-15 10:50:09','Jane','Smith','janesmith@foo.bar','AQAAAAEAACcQAAAAENR+5SkBUhKGcP7YyEMpzBlHoHuzR/Q99xVv+Slu7ARPDMWj5MIMkRQJ0K28w1TzMQ=='),(3,'2017-12-15 13:17:21','2017-12-15 13:17:21','Will','Fletcher','willfletcher@alpha.tck','AQAAAAEAACcQAAAAEAG9Tejb+4T8N7ucA8xSiFPkXyRZtIipTze7UIW1d9OeJ69jLvSR/NUDk0DyvIuhZw=='),(4,'2017-12-15 17:12:20','2017-12-15 17:12:20','Ben','Peters','benpeters97@yahoo.com','AQAAAAEAACcQAAAAEAzFS8u7/YW/6cNaPV8AS+G/YsMz1rYIaqJmUGDe2jb4sJ4J2NcPR3+/Rs9++W8RUQ=='),(5,'2017-12-15 17:14:00','2017-12-15 17:14:00','Some','Guy','someguy@foo.bar','AQAAAAEAACcQAAAAEJIzZjSKKSxpA5lq334vuW86l8gyzRYLTMZ2zowj8OIEScv6fBo1AZqwGGQXn54UUw==');
+INSERT INTO `users` VALUES (1,'2017-12-18 13:13:05','2017-12-18 13:13:05','John Smith','johnsmith@foo.bar','AQAAAAEAACcQAAAAEIJFihh/Wg6/sg6kmAh7t7qRFwvSHjjCtKiqBzh0zu/20zlu74y3pK9AjgORg/wO/g==','I am an extremely generic man, there isn\'t much to say about me that you couldn\'t already intuit.'),(2,'2017-12-18 13:49:34','2017-12-18 13:49:34','Jane Smith','janesmith@foo.bar','AQAAAAEAACcQAAAAEOH7xTW6inLP5hVkl1CxxM0TlNw1mDmXaeiIWH3GV1M6QnO6OqoQ5X0zHKovyi5V8A==','I am an extremely generic woman, there isn\'t much to say about me that you couldn\'t already intuit.'),(3,'2017-12-18 14:09:40','2017-12-18 14:09:40','Will Fletcher','willfletcher@alpha.tck','AQAAAAEAACcQAAAAEGARGtu2JBBbb8YsxmEhx3B6aEckYmMheCrhhERy/I7ZxTWsB5pw+lOLDyuckApteQ==','It is a mystery!'),(4,'2017-12-18 15:08:00','2017-12-18 15:08:00','Ben Peters','benpeters97@yahoo.com','AQAAAAEAACcQAAAAEDcUJUUFKmZ2r9IjYC7i9mHXtrGpQzity3V+lg6kEWZ8qKowW4P20gmAZB2Y5kUMug==','A progrmmer with several years of amateur experience, and a few months of bootcamp.');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -118,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-18 15:22:18
+-- Dump completed on 2017-12-18 15:36:21
